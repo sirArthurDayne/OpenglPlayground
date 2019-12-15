3e036485ddad60f9876de15877eed977bf077138
Code_Krosis <xavierivan2000@hotmail.com>
2019-12-14 21:44:19 -0500

    first function cube. TODO: fix texture behaviour.

--- OpenglPlayground/src/tests/Texture3D.cpp
+++ OpenglPlayground/src/tests/Texture3D.cpp
@@ -18,26 +18,47 @@ test::Texture3D::Texture3D():
 {
 	Vertex tri_pos[] =
 	{
-		{glm::vec3(1.0f, -1.0f, .0f), glm::vec2(1.0f, 0.0f)},//0
-		{glm::vec3(1.0f, 1.0f, .0f), glm::vec2(1.0f, 1.0f)},//1
-		{glm::vec3(-1.0f, -1.0f,.0f), glm::vec2(0.0f, 0.0f)},//2
-		{glm::vec3(-1.0f, 1.0f, .0f), glm::vec2(0.0f, 1.0f)},//3
+		{glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec2(0.0f, 1.0f)},//0
+		{glm::vec3(-1.0f, 1.0f, -1.0f), glm::vec2(1.0f, 0.0f)},//1
+		{glm::vec3(1.0f, 1.0f,-1.0f), glm::vec2(1.0f, 0.0f)},//2
+		{glm::vec3(1.0f, -1.0f, -1.0f), glm::vec2(1.0f, 1.0f)},//3
+		
+		{glm::vec3(-1.0f, -1.0f,1.0f), glm::vec2(0.0f, 0.0f)},//4
+		{glm::vec3(-1.0f,1.0f,1.0f),glm::vec2(1.0f, 0.0f)},//5
+		{glm::vec3(1.0f,1.0f,1.0f),glm::vec2(1.0f, 1.0f)},//6
+		{glm::vec3(1.0f,-1.0f,1.0f),glm::vec2(1.0f, .0f)},//7
 	};
 	unsigned int tri_indices[] =
 	{
-		2,0,1,//top triangle
-		3,2,1,//down triangle
+		//bottom
+		0,1,2,
+		3,2,0,
+		//left
+		0,4,5,
+		0,5,1,
+		//right
+		3,7,6,
+		2,6,3,
+		//back
+		1,5,6,
+		6,1,2,
+		//front
+		0,4,7,
+		7,0,3,
+		//top
+		4,5,6,
+		7,4,6
 	};
-
+	glEnable(GL_DEPTH_TEST);
 	m_VAO = new VertexArray();
 	m_VL = new VertexLayout();
 	m_VL->Push<float>(3);//pos
 	m_VL->Push<float>(2);//texture
 
-	m_VBO = new VertexBuffer(tri_pos,  4 * sizeof(Vertex));
+	m_VBO = new VertexBuffer(tri_pos,  8 * sizeof(Vertex));
 	m_VAO->AddBuffer(*m_VBO, *m_VL);
 
-	m_IBO = new IndexBuffer(tri_indices, 6);
+	m_IBO = new IndexBuffer(tri_indices, 36);
 
 	m_shader = new Shader("shaders/Base.shader");
 	m_shader->SetUniform1i("u_texture0", 0);
@@ -69,7 +90,7 @@ void test::Texture3D::OnRenderer()
 {
 	Renderer renderer;
 	renderer.Clear(0.80f, 0.30f, 0.50f);
-
+	glClear( GL_DEPTH_BUFFER_BIT);
 	//shader binding and uniform sending data
 	m_texture->Bind();
 	m_texture2->Bind(1);
@@ -99,7 +120,7 @@ void test::Texture3D::OnGuiRenderer()
 	// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
 	ImGui::Begin("Move Cube!");
 	ImGui::SliderFloat("FOV", &m_FOV, 30.0f, 120.0f);
-	ImGui::SliderFloat("camera Z", &m_camera.z, -3.0f, -20.0f);
+	ImGui::SliderFloat3("camera X", &m_camera.x, -20.0f, 20.0f);
 	ImGui::SliderFloat3("coordenadas", &m_translationA.x, -10.0f, 10.0f);
 	ImGui::SliderFloat3("escala", &m_scaleVec.x, 0.50f, 3.0);
 	ImGui::SliderFloat("rotacion", &m_rotation, -180.0f, 180.0f);
